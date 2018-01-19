5b44438d3bfe260368cca113f3a9641181a6b9b1
mark adel <mark.adell@outlook.com>
2018-01-19 18:01:42 +0200

    re-implement concat function to work with any passed parameters

--- test/lodasharray.js
+++ test/lodasharray.js
@@ -2,13 +2,7 @@ const expect = require('chai').expect;
 const _ = require('../');
 
 describe('concat function', () => {
-    it('should return the concatination of two arrays', () => {
-        expect(_.concat([1,2], [3,4,5])).to.be.eql([1,2,3,4,5]);
-    });
-    it('should return the passed array if passed with one array', () => {
-        expect(_.concat([1,2,3,4,5])).to.be.eql([1,2,3,4,5]);
-    });
-    it('should return undefined if passed anything other than arrays', () => {
-        expect(_.concat([1,2,3,4], 5)).to.be.eql(undefined);
+    it('should return the concatination of values of passed arrays with any other types', () => {
+        expect(_.concat([1],[2,3],4,{n: 5})).to.be.eql([1,2,3,4,{n: 5}]);
     });
 });
