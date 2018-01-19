899cff55893c931242833cd8ab551ef7b3be3284
mark adel <mark.adell@outlook.com>
2018-01-19 18:03:33 +0200

    implement and test uniq function

--- test/lodasharray.js
+++ test/lodasharray.js
@@ -6,3 +6,9 @@ describe('concat function', () => {
         expect(_.concat([1],[2,3],4,{n: 5})).to.be.eql([1,2,3,4,{n: 5}]);
     });
 });
+
+describe('uniq function', () => {
+    it('should return the array after removing duplications', () => {
+        expect(_.uniq([1, 1, 2, 2, '3', '3', 4, 5, 5])).to.eql([1, 2, '3', 4, 5]);
+    });
+});
