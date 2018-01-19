b81c422d505caba027ab9d66eb90d61409727b30
mark adel <mark.adell@outlook.com>
2018-01-19 19:05:58 +0200

    implement and test range function

--- test/lodasharray.js
+++ test/lodasharray.js
@@ -9,6 +9,20 @@ describe('concat function', () => {
 
 describe('uniq function', () => {
     it('should return the array after removing duplications', () => {
-        expect(_.uniq([1, 1, 2, 2, '3', '3', 4, 5, 5])).to.eql([1, 2, '3', 4, 5]);
+        expect(_.uniq([1,1,2,2,'3','3',4,5,5])).to.eql([1, 2, '3', 4, 5]);
+    });
+});
+
+describe('range function', () => {
+    it('should return an array of numbers within the given range', () => {
+        expect(_.range(1,5)).to.eql([1,2,3,4,5]);
+        expect(_.range(5,1)).to.eql([5,4,3,2,1]);
+    });
+    it('should return the range starting from 0 if passed if one parameter', () => {
+        expect(_.range(3)).to.eql([0,1,2,3]);
+        expect(_.range(-3)).to.eql([0,-1,-2,-3]);
+    });
+    it('should return an empty array if called without parameters', () => {
+        expect(_.range()).to.eql([]);
     });
 });
